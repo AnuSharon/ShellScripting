7ec6c49488dd4c13e6a146d6d8ab730cd139adae
AnuSharon <abaki.1410@gmail.com>
2022-09-15 14:00:30 -0600

    Shebang Topic

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -3,8 +3,8 @@
 #! is called as she-bang
 shebang is completely optional
 adding this will help us to udnerstand what type of script it is
-* For a file only one shebang is possible. If you keep one more shebang in second line then it is considered as comment.
-* Also shebang has to be in very first line.
+For a file only one shebang is possible. If you keep one more shebang in second line then it is considered as comment.
+Also shebang has to be in very first line.
 
 
 
